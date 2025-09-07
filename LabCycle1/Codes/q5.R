0945022bfe6ae551d6313ff52a86b06966b9a03c
Mariya Jyothy <mariyajyothy04@gmail.com>
2025-09-07 23:11:16 +0530

    Create q5.R

---
+++ LabCycle1/Codes/q5.R
@@ -0,0 +1,23 @@
+series_generator <-function(terms){
+  num=1
+  den=1
+  t=(num/den)
+  if (terms==0 || terms==1){
+    return (1)
+  } else{
+    for (i in 2:terms){
+        num=num+1
+        den=num*2-1
+      if (i%%2 ==0){
+
+        t=t-(num/den)
+      }
+      else{
+        t=t+(num/den)
+      }
+    }
+  }
+  return(t)
+}
+t=as.numeric(readline(prompt="Enter the number of terms:"))
+cat("The sum of the series: 1 - 2/3 + 3/5 - 4/7 +.... upto",t,"is:",series_generator(t))
