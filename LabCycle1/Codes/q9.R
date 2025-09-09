088d390e53ecf58ca062c628101f1f2ad28d6839
Mariya Jyothy <mariyajyothy04@gmail.com>
2025-09-09 22:31:42 +0530

    Create q9.R

---
+++ LabCycle1/Codes/q9.R
@@ -0,0 +1,20 @@
+compression <- function(string){
+  
+  occurence_vector <-c()
+  string_vector <-unlist(strsplit(string,""))
+  for (i in (1:length(string_vector))){
+    occurrence <-0
+    char <-string_vector[i]
+    if(char %in% occurence_vector){
+      next
+    }
+    for (j in (1:length(string_vector))){
+      if (char==string_vector[j]){
+        occurrence=occurrence+1
+      }
+    }
+    occurence_vector <-c(occurence_vector," ",char,":",occurrence)
+  }
+  return(occurence_vector)
+}
+cat(compression("She speaks four languages"))
